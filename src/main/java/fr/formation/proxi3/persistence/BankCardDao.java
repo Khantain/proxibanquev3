b30686879d0a3459e980499af501130681d4d285
Croco-deal <sarah.ourabah@yahoo.fr>
2018-12-17 14:19:17 +0100

    DAO suite

--- src/main/java/fr/formation/proxi3/persistence/BankCardDao.java
+++ src/main/java/fr/formation/proxi3/persistence/BankCardDao.java
@@ -1,5 +1,31 @@
 package fr.formation.proxi3.persistence;
 
-public class BankCardDao {
+import java.util.List;
+
+import fr.formation.proxi3.metier.entity.BankCard;
+
+public class BankCardDao extends AbstractDao<BankCard>{
+	
+	private static final BankCardDao INSTANCE = new BankCardDao();
+	
+	public static BankCardDao getInstance() {
+		return BankCardDao.INSTANCE;
+	}
+
+	@Override
+	public BankCard read(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BankCard> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public BankCard create(BankCard bankcard) {
+		return null;
+	}
 
 }
